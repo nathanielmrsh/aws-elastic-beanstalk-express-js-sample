1bb602c7b0668ebb32d6cb95c40dde78526b883f
nate <50018049@serc.ac.uk>
2025-02-18 13:06:53 +0000

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I hate everything!'));
+app.get('/', (req, res) => res.send('I really like everything HAHAHAH!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
